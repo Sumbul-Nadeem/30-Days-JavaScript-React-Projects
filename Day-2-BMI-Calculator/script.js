1b11cc85df9bdad6284e05b345d89f6f9196ab07
Sumbul-Nadeem <sumbuln13@gmail.com>
2025-08-03 23:44:29 +0530

    Day-2

---
+++ Day-2-BMI-Calculator/script.js
@@ -0,0 +1,37 @@
+function BMICalculator(weight, height) {
+  let bmi = weight / (height * height);
+  bmi = bmi.toFixed(1); // round to 1 decimal
+
+  let category = "";
+
+  if (bmi < 18.5) {
+    category = "Underweight";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    category = "Normal";
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    category = "Overweight";
+  } else if (bmi >= 30) {
+    category = "Obese";
+  } else {
+    category = "Invalid values";
+  }
+
+  return { bmi, category };
+}
+
+function calculateBMI() {
+  const weight = parseFloat(document.getElementById("weight").value);
+  const height = parseFloat(document.getElementById("height").value);
+  const resultEl = document.getElementById("result");
+
+  if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
+    resultEl.innerText = "Please enter valid numbers.";
+    resultEl.classList.add("show");
+    return;
+  }
+
+  const { bmi, category } = BMICalculator(weight, height);
+
+  resultEl.innerText = `Your BMI is ${bmi} (${category})`;
+  resultEl.classList.add("show");
+}
